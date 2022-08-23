01607889f9ad75763937936325877c884eefe2f4
Pierre Gauthier <pigau@smile.fr>
2022-08-23 14:18:48 +0200

    Initial SmileLab Configuration for Magento2

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Smile Lab PHPStan Coding Standards
 
 These coding standards are meant to be used on any community project made by Smile Lab.
+
+First, you need to create a phpstan configuration file in your root project directory : 
+```neon
+# phpstan.neon.dist
+parameters:
+    level: 6
+    checkMissingIterableValueType: false
+    excludePaths:
+        - 'vendor/*'
+```
+
+You can run the phpstan analysis with this command :
+```bash
+php ./vendor/bin/phpstan analyze [src folder]
+```
+
+Please note that the src folder is copied from `https://github.com/magento/magento2/tree/2.4.4/dev/tests/static/framework/Magento/PhpStan`. Only the namespace has been changed. We have copied these source files in order to avoid to depend on the Magento sources to run these checks.
---
+++ src/autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copied from magento.
+ * @sse https://github.com/magento/magento2/blob/2.4-develop/dev/tests/static/framework/Magento/PhpStan/autoload.php
+ * phpcs:disable
+ */
+
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Code\Generator\Io;
+use Magento\Framework\Filesystem\Driver\File;
+use Magento\Framework\TestFramework\Unit\Autoloader\ExtensionAttributesGenerator;
+use Magento\Framework\TestFramework\Unit\Autoloader\ExtensionAttributesInterfaceGenerator;
+use Magento\Framework\TestFramework\Unit\Autoloader\FactoryGenerator;
+use Magento\Framework\TestFramework\Unit\Autoloader\GeneratedClassesAutoloader;
+
+require realpath(__DIR__ . '/../../../../') . '/vendor/squizlabs/php_codesniffer/autoload.php';
+
+if (!defined('GENERATED_TEMP_DIR')) {
+    define('GENERATED_TEMP_DIR', dirname(__DIR__) . '/tmp');
+}
+
+$generatorIo = new Io(
+    new File(),
+    GENERATED_TEMP_DIR . '/' . DirectoryList::getDefaultConfig()[DirectoryList::GENERATED_CODE][DirectoryList::PATH]
+);
+$generatedCodeAutoloader = new GeneratedClassesAutoloader(
+    [
+        new ExtensionAttributesGenerator(),
+        new ExtensionAttributesInterfaceGenerator(),
+        new FactoryGenerator(),
+    ],
+    $generatorIo
+);
+spl_autoload_register([$generatedCodeAutoloader, 'load']); // @phpstan-ignore-line
+
